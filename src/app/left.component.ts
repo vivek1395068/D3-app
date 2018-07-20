2c5109930445cbeeb5076cdf93c880b416fc8b85
Vivek <visingh0@in.ibm.com>
2018-07-20 12:54:00 +0530

    d3 app

---
+++ src/app/left.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'leftPanel',
+  template:`<div class="leftPanel">
+  leftPanel
+            </div>`,
+  //templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class LeftPanelComponent {
+  //title = 'app';
+}
